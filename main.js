4718bffc9864adfd3072d2a04b5c3f4844b2f016
KBBOtherspaces <kristinbauerart@gmail.com>
2025-11-17 19:11:27 -0800

    add 3 cubes- inital commit

--- main.js
+++ main.js
@@ -20,11 +20,27 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-geometry.translate(0, 0, 0.5);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// cube 1
+const geometry1 = new THREE.BoxGeometry(1, 1, 1);
+geometry1.translate(0, 0, 0.5);
+const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube1 = new THREE.Mesh(geometry1, material1);
+scene.add(cube1);
+
+// cube 2
+const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+geometry2.translate(2, 0, 0.5);
+const material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const cube2 = new THREE.Mesh(geometry2, material2);
+scene.add(cube2);
+
+// cube 3
+const geometry3 = new THREE.BoxGeometry(1, 1, 1);
+geometry3.translate(4, 0, 0.5);
+const material3 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube3 = new THREE.Mesh(geometry3, material3);
+scene.add(cube3);
+
 
 camera.position.z = 5;
 camera.position.y = 7;
